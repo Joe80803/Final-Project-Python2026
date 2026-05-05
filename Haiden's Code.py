c28ea0da248c48d0d0d4956a51f99a287eb8b198
zHYoder44 <haidenyoder27@gmail.com>
2026-05-05 14:05:47 -0400

    I created the collection with some of the brainrots.

---
+++ Haiden's Code.py
@@ -0,0 +1,8 @@
+import random
+brainrots = [("Ballerina Cappuccina"), ("Blueberrinni Octopussini"),("Bobrito Bandito"), ("Bombardiro Crocodilo"), ("Bombombini Gusini"),
+             ("Boneca Ambalabu"), ("Brr Brr Patapim"), ("Brri Brri Bicus Dicus Bombicus"), ("Burbaloni Lulilolli"), ("Cappuccino Assassino"),
+             ("Chef Crabracadabra"),("Chimpanzini Bananini"), ("Cocofanto Elefanto"), ("Frigo Camelo"), ("Frulli Frulla"),("Gangster Footera"),
+             ("Garam Mararam & Madu Dung Dung"), ("Giraffa Celeste"), ("Glorbo Fruttodrillo"), ("Graipussi Medussi"), ("Il Cacto Hipopotamo"),
+             ("La Vaca Saturno Saturnita"), ("Lirili Larila"), ("Orangutini Ananasini"), ("Pot Hotspot"), ("Rhino Toasterino"), ("Ta Ta Ta Sahur"),
+             ("Tigrrullini Watermellini"),("Tracotucotulu Delapeladustuz"),("Tralalero Tralala"),("Tric Trac Baraboom"),("Trippi Troppi"),
+             ("Trulimero Trulicina"),("Tung Tung Tung Sahur"),("U Din Din Din Din Dun"),("Zibra Zubra Zibralini")]
