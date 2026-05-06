be6b7dd77ead982f97b9ed6d891d0669f9bc2a20
zjmajor <adsvcdsvfdsc@>
2026-05-06 14:14:31 -0400

    I have fixed the problems with the code I had yesterday and so I can now begin to format the visuals for each question besides the background.

--- Joey's code.py
+++ Joey's code.py
@@ -1,22 +1,29 @@
 import tkinter as tk
 from PIL import ImageTk, Image
-
 window = tk.Tk()
+import requests
+
+# imports the BeautifulSoup library, that lets us format site info in a readable way.
+from bs4 import BeautifulSoup
+
+# takes the current site address we want to use and stores it as a variable called "URL"
 
 window.title("AMAZING guess the brainrot quiz!")
 
 window.geometry("500x500")
 
-background = tk.Image(r"C:\Users\zjmajor\OneDrive - Hatboro-Horsham School District\steal a brainrot base.jpeg")
+image = Image.open(r"C:\Users\zjmajor\OneDrive - Hatboro-Horsham School District\steal a brainrot base.jpeg")
 
-label = tk.Label(
-    text="",
-    fg="",
-    bg="",
-    width=25,
-    height=5
-)
+resizedImage = image.resize((600,600))
 
-label.place(x=150, y=50)
+photo = ImageTk.PhotoImage(resizedImage)
+
+label1 = tk.Label(image=photo)
+
+label1.image = photo
+
+label1.place(x=-50,y=-50)
+
+label2 = tk.Label()
 
 window.mainloop()
